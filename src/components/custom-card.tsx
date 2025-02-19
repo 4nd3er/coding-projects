bf530ecb748db3e22337f52c1853d26eec1c8f5f
marthacruzz <marthacruz@zaphreen.com>
2025-02-19 12:23:49 -0500

    añadimos funcionalidad de cards

--- src/components/custom-card.tsx
+++ src/components/custom-card.tsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
-import { CharacterType } from "../pages/character-detail";
 import { Card } from "./ui/card";
 import { Github } from "../components/Github.tsx";
 import { Youtube } from "../components/Youtube.tsx";
 import { Web } from "../components/Web.tsx";
+
+type CharacterType = {
+  id: string;
+  name: string;
+  species?: string;
+  image?: string;
+  url?: string;
+  description: string;
+  links: Record<any, any>[];
+};
 
 const CustomCard = ({
   data,
@@ -22,27 +31,34 @@ const CustomCard = ({
   };
   return (
     <div
-      className="relative w-[350px] sm:w-[420px] mx-auto hover:scale-105 overflow-hidden transition"
+      className="relative sm:w-[480px] w-[350px] mx-auto hover:scale-105 overflow-hidden transition"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <Card className="bg-gradient-to-r from-cyan-500 to-blue-500 overflow-hidden">
-        <img src={data?.image} className="mx-auto w-full" alt="" />
+        <img src={data?.image} className="w-full h-auto aspect-video object-cover" alt="" />
         <div
-          className={`text-white z-20 absolute bottom-3 left-4 transition font-semibold ${
-            data?.id === id
-              ? "translate-y-0"
-              : "translate-y-[calc(100%_-_2rem)]"
-          } flex flex-col gap-4`}
+          className={`text-white z-20 absolute bottom-3 left-4 transition font-semibold ${data?.id === id
+            ? "translate-y-0"
+            : "translate-y-[calc(100%_-_1.5rem)]"
+            } flex flex-col gap-3`}
         >
           <p className="text-white font-semibold">{data?.name}</p>
-          <p className={`text-sm h-auto pr-2 text-white text-pretty`}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. tis ad
-            ratione adipisci, iure rerum, dolor culpa, asperiores rem
-            praesentium dolores? Id non reiciendis sunt?
+          <p className={`text-sm h-auto  text-white text-pretty`}>
+            {data?.description}
           </p>
-          <div className="flex gap-4">
-            <Link
+          <div className="flex gap-4 over">
+            {data?.links.map(link => (
+              <Link
+                to={link.link}
+                target="_blank"
+                className="text-white border font-semibold flex gap-2 p-1 rounded-md"
+              >
+                {link.component}
+                {link.name}
+              </Link>
+            ))}
+            {/* <Link
               to=""
               target="_blank"
               className="text-white border font-semibold flex gap-2 p-1 rounded-md"
@@ -65,10 +81,13 @@ const CustomCard = ({
             >
               <Web />
               Web
-            </Link>
+            </Link> */}
           </div>
         </div>
-        <div className="absolute inset-0 w-full h-full bg-gradient rounded-md"></div>
+        <div className={`absolute inset-0 w-full h-full rounded-md ${data?.id === id
+          ? "bg-gradient_h"
+          : "bg-gradient"
+          }`}></div>
       </Card>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ export default {
       },
       backgroundImage: {
         gradient: "linear-gradient(transparent 70%,black)",
+        gradient_h: "linear-gradient(transparent 50%,black)",
       },
       colors: {
         background: "hsl(var(--background))",
